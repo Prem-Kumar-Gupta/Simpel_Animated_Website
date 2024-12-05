f64c33540e6bda4f2d6fdf2b24999dc4cd975ec8
Prem-Kumar-Gupta <premgupta1629@gmail.com>
2024-12-05 19:32:40 +0530

    Page1 Completed

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  height: "110px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    start:"top -10%",
+    end:"top -11%",
+    scrub:1
+  },
+});
