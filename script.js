64cff722bd53e9b42b61a0ead0f5e8ceddfc9b7b
Prem-Kumar-Gupta <premgupta1629@gmail.com>
2024-12-07 18:10:01 +0530

    completed page1 style

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const cursor = document.querySelector("#cursor");
+const cursorBlur = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.x + "px";
+  cursor.style.top = e.y + "px";
+  cursorBlur.style.left = e.x - 200 + "px";
+  cursorBlur.style.top = e.y - 200 + "px";
+});
+
 gsap.to("#nav", {
   backgroundColor: "#000",
   height: "110px",
@@ -5,8 +14,19 @@ gsap.to("#nav", {
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    start:"top -10%",
-    end:"top -11%",
-    scrub:1
+    start: "top -10%",
+    end: "top -11%",
+    // markers:true,
+    scrub: 1,
+  },
+});
+gsap.to("#main", {
+  backgroundColor: "black",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 1,
   },
 });
